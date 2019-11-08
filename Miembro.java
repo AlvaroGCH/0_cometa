0a9a8f2858744dc43503c13a1bc4df114f984b81
AlvaroGCH <57445055+AlvaroGCH@users.noreply.github.com>
2019-11-08 16:42:12 +0100

    Add files via upload

---
+++ Miembro.java
@@ -0,0 +1,68 @@
+
+public class  Miembro {
+	private String nombre;
+	private String apellidos;
+	private Cargo cargo;
+	private int experiencia;
+	private int nacimiento;  
+	private double habilidad;
+	private Departamento departamento;
+	
+	public Miembro(String nombre, String apellidos, Cargo cargo, int experiencia, int nacimiento, double habilidad, Departamento departamento){
+		this.nombre = String.valueOf(nombre);
+		this.apellidos = String.valueOf(apellidos);
+		this.cargo = cargo;
+		this.experiencia = experiencia;
+		this.nacimiento = nacimiento;
+		this.habilidad = habilidad;
+		this.departamento = departamento;
+	}
+	
+	public void setNombre(String nombre){{
+		this.nombre = String.valueOf(nombre);
+	}
+	
+	public void setApellidos(String apellidos){{
+		this.apellidos = String.valueOf(apellidos);
+	}
+	
+	/*public void setCargo(String cargo){{ 		
+		
+	}*/
+	
+	public void setExperiencia(int experiencia){{ 		
+		this.experiencia = experiencia;
+	}
+	
+	public String getNombre(){
+		return nombre;
+	}
+	
+	public String getApellidos(){
+		return apellidos;
+	}
+	
+	public Cargo getCargo(){
+		return cargo;
+	}
+	
+	public int getExperiencia(){
+		return experiencia;
+	}
+	
+	public int getNacimiento(){			
+		return nacimiento;
+	}
+	
+	public int getEdad() {
+		return 2061-nacimiento;
+	}
+	
+	public double getHabilidad(){
+		return habilidad;
+	}
+	
+	public Departamento getDepartamento(){
+		return departamento;
+	}
+}
